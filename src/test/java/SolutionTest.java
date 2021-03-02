59fb7365c9204df577f76dd8dd4c88c6e397333e
Nirmit Goyal <nirgoyal@amazon.com>
2021-03-02 15:55:16 +0530

    add junit5

---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    @Test
+    void fun() {
+        Assertions.assertEquals(-1, Solution.fun());
+    }
+}
