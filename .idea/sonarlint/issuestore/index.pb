d2e3ad2c91f26356f22e9c0596c0a4c7b290a486
Nirmit <nirmit_goyal@intuit.com>
2021-12-10 12:29:10 +0530

    next

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+K
+src/main/java/Solution.java,2/6/26b1745296c4730ead1a40cbba1b589e8ef778ae
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,15 +1,18 @@
+import java.io.*;
+import java.util.*;
+
 /*
 
 
  */
 public class Solution {
 
-    public static int fun() {
-        //base
-        return -1;
-    }
+  public static int fun() {
+    //base
+    return -1;
+  }
 
-    public static void main(String[] args) {
-        System.out.println(fun());
-    }
+  public static void main(String[] args) {
+    System.out.println(fun());
+  }
 }
